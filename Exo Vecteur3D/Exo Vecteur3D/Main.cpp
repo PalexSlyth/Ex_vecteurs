0eb4155a8275c29b33d1623143c3a68b0137d808
Cyril SIMON <cyril.simon67@gmail.com>
2020-10-12 14:46:59 +0200

    Exercice 69

---
+++ Exo Vecteur3D/Exo Vecteur3D/Main.cpp
@@ -0,0 +1,11 @@
+#include "Vecteur3D.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	Vecteur3D vect;
+	//vect.affichevecteur();
+	return 0;
+}
---
+++ Exo Vecteur3D/Exo Vecteur3D/Vecteur3D.cpp
@@ -0,0 +1,23 @@
+#include "Vecteur3D.h"
+#include <iostream>
+using namespace std;
+
+
+/*Vecteur3D::Vecteur3D()
+{
+	m_fltX = 0;
+	m_fltY = 0;
+	m_fltZ = 0;
+}*/
+
+/*Vecteur3D::Vecteur3D(float X, float Y, float Z)
+{
+	m_fltX = X;
+	m_fltY = Y;
+	m_fltZ = Z;
+}
+
+/*void Vecteur3D::affichevecteur()
+{
+	cout << this->m_fltX << this->m_fltY << this->m_fltZ << endl;
+}*/
